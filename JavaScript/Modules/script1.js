4a6333bbbd9059aeb22014059ddfa4185e2bcbcd
Nikhil09-hub <bandarunikhil48@gmail.com>
2026-02-01 21:48:46 +0530

    Modules in Javascript

---
+++ JavaScript/Modules/script1.js
@@ -0,0 +1,10 @@
+//named export->importing module should use the same name
+export let a = 100
+export let username = 'Madhu'
+export let findSum=(m,n)=>{
+    return m+n
+} 
+//or export {a,username,findSum} 
+//default export 
+//export default {a,username,findSum};
+ 
---
+++ JavaScript/Modules/script2.js
@@ -0,0 +1,14 @@
+//importing a defalut export
+//import x from './script1.js'
+
+ 
+//console.log(x);
+//console.log(x.a,x.username);
+//console.log(x.findSum(100,200));
+
+
+//importing a named export
+import {a,username,findSum} from './script1.js'
+console.log(a);
+console.log(username);
+console.log(findSum(10,8));
