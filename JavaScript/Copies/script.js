b441a0abda9a29c4ac08953556fc70e581ce6d37
Nikhil09-hub <bandarunikhil48@gmail.com>
2026-02-04 23:10:14 +0530

    Deep and Shallow Copies

--- JavaScript/Copies/script.js
+++ JavaScript/Copies/script.js
@@ -12,12 +12,13 @@ let person = {
     pid:100,
     name:'Anand'
 }
+//Deep Copy
 //Spread operator ... should be used if not copies for refereces are made not for the objects
 let copyPerson = {...person}
 console.log("Person Object :",person);
 console.log("Perosn object copy",copyPerson);
 
-//deepcopy for nested objects
+//Shallow Copy  for nested objects
 let emp = {
     empId:100,
     name:'ravi',
@@ -26,7 +27,9 @@ let emp = {
         pincode:500037
     }
 }
-let copyEmp = {...emp}
+
+//let copyEmp = {...emp} (Not usefull)
+let copyEmp = structuredClone(emp)
 emp.empId = 123
 emp.address.city = 'chennai'
 console.log("emp :",emp);
---
+++ JavaScript/Destructure/script.js
@@ -0,0 +1,9 @@
+//packing 
+    //array 
+        let arr = [1,2,3,4]
+
+    //object
+        let obj = {
+            a:10,
+            b:20
+        }
