a73127818f4ac9342df927f2048ece22a86ee829
Nikhil09-hub <bandarunikhil48@gmail.com>
2026-02-05 22:45:24 +0530

    Packing and Unpacking

--- JavaScript/Destructure/script.js
+++ JavaScript/Destructure/script.js
@@ -7,3 +7,19 @@
             a:10,
             b:20
         }
+//Unpacking(Destructing)
+    //array
+    console.log(arr[0])
+    console.log(arr[1])
+
+    let [m,n,p,q]=arr;
+    console.log(m)
+    console.log(q)
+    
+    //object
+    console.log(obj.a)
+    console.log(obj.b)
+
+    let {a,b}=obj;
+    console.log(a)
+    console.log(b)
