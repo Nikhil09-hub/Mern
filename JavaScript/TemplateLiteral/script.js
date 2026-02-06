44d36077cd932ae5628351be9943db22a21b0ba8
Nikhil09-hub <bandarunikhil48@gmail.com>
2026-02-06 23:34:16 +0530

    Template Literal

---
+++ JavaScript/TemplateLiteral/script.js
@@ -0,0 +1,5 @@
+let a = 10;
+let b = 20;
+let c = 30;
+console.log("Value of a is :",a,"Value of b is :",b,"Value of c is :",c);
+console.log(`Value of a is :${a} Value of b is ${b} Value of c is : ${c}`);
