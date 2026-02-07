80fb196ce04f2c41c25c7ef40ac2200fa2a0ef32
Nikhil09-hub <bandarunikhil48@gmail.com>
2026-02-07 22:56:31 +0530

    Debugging in java

---
+++ JavaScript/Debug.js/script.js
@@ -0,0 +1,13 @@
+//inspect sources for html file and create breakpoint to stop excecution at the break points
+console.log('first');
+console.log('second');
+console.log('third');
+//remove breakpoints after debugging
+ function findSum(a,b){
+    let sum = a+b
+    return sum
+}
+
+//functions needs to be called to excecute
+let result = findSum(10,20)
+console.log(result);
