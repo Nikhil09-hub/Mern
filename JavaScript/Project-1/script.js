62b239cbb4910ab26e5e03a13b750e1923565d18
Nikhil09-hub <bandarunikhil48@gmail.com>
2026-02-11 22:26:06 +0530

    Getting elements from DOM

---
+++ JavaScript/Project-1/script.js
@@ -0,0 +1,8 @@
+//Get elements from the DOM
+    //Read head 
+        console.log(document.head);
+    //Read body 
+        console.log(document.body);
+    //Read other elements
+        heading = document.querySelector('h1')
+        console.log(heading);
