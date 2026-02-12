48c77b655f340b3abfeb17c20ae8646d68aef24a
Nikhil09-hub <bandarunikhil48@gmail.com>
2026-02-12 22:57:40 +0530

    Getting elements from DOM extended

--- JavaScript/Project-1/script.js
+++ JavaScript/Project-1/script.js
@@ -6,3 +6,10 @@
     //Read other elements
         heading = document.querySelector('h1')
         console.log(heading);
+        //For more thanone element of same time 
+        allHeadings = document.querySelectorAll('h1')
+        console.log(allHeadings);
+        console.log(allHeadings[0]);
+        //Using class
+        classHeading = document.querySelectorAll('.header')
+        console.log(classHeading); // Same for id using #
