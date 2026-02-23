3996468cbcb394badf23b645237ff5c3baefe6a5
Nikhil09-hub <bandarunikhil48@gmail.com>
2026-02-23 22:15:12 +0530

    Different product  components

--- React/first-app/src/components/Product.jsx
+++ React/first-app/src/components/Product.jsx
@@ -2,7 +2,7 @@ function Product(props){ // {data:{}}
 
 
     return (
-        <div className="w-25 shadow-lg mx-auto">
+        <div className="w-25 shadow-lg mx-auto bg-info">
             <p className="fs-1">{props.data.pid}</p>
             <p className="fs-1">{props.data.pname}</p>
             <p className="fs-1">{props.data.price}</p>
