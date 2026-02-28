0eb6a31e62d42a792e13c1c5361adf9a6474d0f2
Nikhil09-hub <bandarunikhil48@gmail.com>
2026-02-28 23:19:30 +0530

    Change state for objects

--- React/first-app/src/components/StateChange.jsx
+++ React/first-app/src/components/StateChange.jsx
@@ -10,6 +10,10 @@ function StateChange(){
     const userNameChange=()=>{
         setUsername('Ravi Kumar')
     }
+    let [student,setStudent]=useState({sno:100,name:'Bhanu',city:'hyd'})
+    const changeStudent=()=>{
+        setStudent({...student,name:'Bhanu prasad'})
+    }
     return(
         <div>
            <h1 className="text-info  bg-white display-3">State Demo</h1> 
@@ -17,6 +21,10 @@ function StateChange(){
            <button className="btn btn-sm btn-info" onClick={()=>{counterIncrement()}}>Click</button>
            <h1 className="text-danger display-4">{username}</h1>
            <button className="btn btn-primary btn-sm" onClick={()=>{userNameChange()}}>ChangeName</button>
+           <h2>{student.sno}</h2>
+           <h2>{student.name}</h2>
+           <h2>{student.city}</h2>
+           <buton className="btn btn-success btn-sm" onClick={()=>{changeStudent()}}>Change Student</buton>
         </div>
     )
 }
